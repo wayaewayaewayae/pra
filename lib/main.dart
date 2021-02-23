66fd9e72f6315bab25174cfc724f81dd2116d7e9
wayaewayaewayae <santiatia2003@gmail.com>
2021-02-23 07:48:51 +0700

    coba

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import 'package:provider/provider.dart';
+import './providers/corona_provider.dart';
+import './home.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => CoronaProvider(),
+        )
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Home(),
+        title: 'Covid-19',
+      ),
+    );
+  }
+}
---
+++ lib/providers/world_model.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Worldmodel {
+  final String positif;
+  final String sembuh;
+  final String meninggal;
+
+  WorldModel({
+    @required this.positif,
+    @required this.sembuh,
+    @required this.meninggal,
+  });
+}
